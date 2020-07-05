6a75ae3cdcfa507da2ae3df9294e58dc779d99ba
master <balazsnoe@gmail.com>
2020-07-05 15:29:28 +0300

    Commit

--- app/src/main/java/com/example/callingkotlinrepositories/data/Repository.kt
+++ app/src/main/java/com/example/callingkotlinrepositories/data/Repository.kt
@@ -1,5 +1,7 @@
 package com.example.callingkotlinrepositories.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Repository(
@@ -7,8 +9,34 @@ data class Repository(
     @field:SerializedName("name") val name: String? = null,
     @field:SerializedName("full_name") val full_name: String? = null,
     @field:SerializedName("description") val description: String? = null
-) {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(id)
+        writeString(name)
+        writeString(full_name)
+        writeString(description)
+    }
+
     override fun toString(): String {
         return "Repository: id = $id, name = $name, full_name = $full_name, description = $description"
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Repository> {
+        override fun createFromParcel(parcel: Parcel): Repository {
+            return Repository(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Repository?> {
+            return arrayOfNulls(size)
+        }
     }
 }
